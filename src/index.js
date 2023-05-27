01c963fd9b6aa2aea1dbb24ad55abd42a6e7f389
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-05-28 01:09:40 +0200

    initial home screen design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles/style.scss'
+import flightLogo from './assets/ft_logo.png'
+
+const logo = document.getElementById('logo');
+logo.src = flightLogo;
+
+// variables
+let flightButton = document.getElementById('find-flights-btn');
+
+
+
+// event listeners
