b8fb7a1d938d9ab303261224bc09506229416898
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-06-01 13:33:45 +0200

    refactoring, extracted runFlightTracker function into its own module

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import './styles/style.scss'
 import flightLogo from './assets/ft_logo.png'
 import pikachu from './assets/pikachu.gif';
-import { populateTable } from './icaoTable.js'
-import { myMap } from './flightLocation';
+import { runFlightTracker } from './js/modules/flightTracker.js';
 
 const logo = document.getElementById('logo');
 logo.src = flightLogo;
 const pikachuLoadingGif = document.getElementById('pikachu-gif');
 pikachuLoadingGif.src = pikachu;
-
-async function runFlightTracker() {
-    try {
-
-        await populateTable();
-        myMap();
-
-    } catch (error) {
-
-        console.error(error);
-    }
-}
 
 runFlightTracker();
 
