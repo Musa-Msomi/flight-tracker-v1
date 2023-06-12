75d685cd63b0b142e60885ab24fd7263019c4273
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-06-12 23:17:20 +0200

    feat: :construction: used tailwind to style search input field

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import './styles/style.scss';
-import flightLogo from './assets/ft_logo.png';
-import pikachu from './assets/pikachu.gif';
-import { runFlightTracker } from './ts/modules/flightTracker';
+import "./styles/style.scss";
+import flightLogo from "./assets/ft_logo.png";
+import pikachu from "./assets/pikachu.gif";
+import { runFlightTracker } from "./ts/modules/flightTracker";
 
-const logo = document.getElementById('logo') as HTMLImageElement;
+const logo = document.getElementById("logo") as HTMLImageElement;
 logo.src = flightLogo;
-const pikachuLoadingGif = document.getElementById('pikachu-gif') as HTMLImageElement;
+const pikachuLoadingGif = document.getElementById(
+  "pikachu-gif"
+) as HTMLImageElement;
 pikachuLoadingGif.src = pikachu;
 
 runFlightTracker();
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Checking for overflowing elements");
+  var docWidth = document.documentElement.offsetWidth;
+  var docHeight = document.documentElement.offsetHeight;
+  var elements = document.querySelectorAll("*");
+  [].forEach.call(elements, function (el: HTMLElement) {
+    if (el.offsetWidth > docWidth || el.offsetHeight > docHeight) {
+      console.log(el);
+      console.log("found the bastards")
+    }
+  });
+});
--- src/ts/modules/api/api.ts
+++ src/ts/modules/api/api.ts
@@ -1,26 +1,31 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from "axios";
 
 export async function getFlights(): Promise<any[]> {
-    const getFlightsUrl = `https://opensky-network.org/api/states/all`;
-    try {
-        const response: AxiosResponse<any> = await axios.get(getFlightsUrl);
-        return response.data.states;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+  const getFlightsUrl = `https://opensky-network.org/api/states/all`;
+  try {
+    const response: AxiosResponse<any> = await axios.get(getFlightsUrl);
+    return response.data.states;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export function getFlightData(icao24: string): Promise<any> {
-    const apiUrl = `https://opensky-network.org/api/tracks/all?icao24=${icao24}&time=0`;
-    return axios.get(apiUrl)
-        .then((response: AxiosResponse<any>) => response.data)
-        .catch((error: any) => {
-            if (error.response && error.response.status === 404) {
-                throw new Error(`Oops, we couldn't find flight ${icao24}'s location`);
-            } else {
-                console.error(error);
-                throw error;
-            }
-        });
+  const apiUrl = `https://opensky-network.org/api/tracks/all?icao24=${icao24}&time=0`;
+  return axios
+    .get(apiUrl)
+    .then((response: AxiosResponse<any>) => response.data)
+    .catch((error: any) => {
+      if (error.response && error.response.status === 404) {
+        throw new Error(`Oops, we couldn't find flight ${icao24}'s location`);
+      } else if (error.response && error.response.status === 500) {
+        throw new Error(
+          "Sorry, something went wrong on our side. We're working on fixing it"
+        );
+      } else {
+        console.error(error);
+        throw error;
+      }
+    });
 }
