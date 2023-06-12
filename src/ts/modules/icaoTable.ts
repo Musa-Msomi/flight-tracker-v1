ec98d6e0f8665e405f6acefeffc5e838ff7dd820
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-06-12 17:05:23 +0200

    feat: :sparkles: added five minute poll

--- src/ts/modules/icaoTable.ts
+++ src/ts/modules/icaoTable.ts
@@ -1,5 +1,11 @@
-import { getFlights } from './api/api';
-import { showLoadingGif, hideLoadingGif, populateTable } from './dom-manipulation/dom-manipulation';
+import { getFlights } from "./api/api";
+import {
+  showLoadingGif,
+  hideLoadingGif,
+  populateTable,
+} from "./dom-manipulation/dom-manipulation";
+import { from, interval } from "rxjs";
+import { switchMap, tap } from "rxjs/operators";
 
 let flightNumbers: string[] = [];
 
@@ -7,26 +13,41 @@ export async function loadAndPopulateTable(): Promise<void> {
   try {
     showLoadingGif();
 
-    const flightDataLocal = localStorage.getItem('flight-numbers');
+    const flightDataLocal = localStorage.getItem("flight-numbers");
     console.log(localStorage);
-    console.log(typeof flightDataLocal)
+    console.log(typeof flightDataLocal);
 
     if (flightDataLocal) {
       flightNumbers = JSON.parse(flightDataLocal);
     } else {
-      const ongoingFlights = await getFlights();
-      flightNumbers = ongoingFlights.map((flight) => flight[0]);
-      localStorage.setItem('flight-numbers', JSON.stringify(flightNumbers));
+      await fetchFlightsAndSaveToLocalStorage();
     }
 
     console.log(flightNumbers);
     populateTable(flightNumbers);
 
     hideLoadingGif();
+
+    const pollMinutesInMilliseconds = 5 * 60 * 1000;
+
+    const interval$ = interval(pollMinutesInMilliseconds);
+
+    interval$
+      .pipe(
+        tap(() => console.log("poll started")),
+        switchMap(() => from(fetchFlightsAndSaveToLocalStorage())),
+        tap(() => console.log("poll finished"))
+      )
+      .subscribe();
   } catch (error) {
     console.error(error);
     throw error;
   }
+}
+async function fetchFlightsAndSaveToLocalStorage(): Promise<void> {
+  const ongoingFlights = await getFlights();
+  flightNumbers = ongoingFlights.map((flight) => flight[0]);
+  localStorage.setItem("flight-numbers", JSON.stringify(flightNumbers));
 }
 
 export function filterFlightsTable(searchValue: string): void {
@@ -34,14 +55,15 @@ export function filterFlightsTable(searchValue: string): void {
     number.toLowerCase().includes(searchValue.toLowerCase())
   );
 
-  const flightRows = document.querySelectorAll<HTMLTableRowElement>('#flights-tbody tr');
+  const flightRows =
+    document.querySelectorAll<HTMLTableRowElement>("#flights-tbody tr");
 
   flightRows.forEach((row) => {
-    const flightNumber = row.querySelector('td')?.textContent;
+    const flightNumber = row.querySelector("td")?.textContent;
     if (flightNumber && flightsSearchedFor.includes(flightNumber)) {
-      row.style.display = 'table-row';
+      row.style.display = "table-row";
     } else {
-      row.style.display = 'none';
+      row.style.display = "none";
     }
   });
 }
