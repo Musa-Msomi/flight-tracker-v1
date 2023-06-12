6130abda32250904a6c725c901b0ca55b1c2eb9a
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-06-13 01:01:56 +0200

    chore: added custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        '150': '150px',
+      },
+    },
   },
   plugins: [],
 }
